27c03616382c87026ca28e5640f3b77e43236d23
kineticskunk <bonga@kineticskunk.com>
2018-10-18 10:17:09 +0200

    added mocha

---
+++ mocha/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Maths calculation', function(){
+    it('should return the correct value when two numbers are added', function(){
+        var result = 1 + 1;
+
+        assert.equal(result, 2);
+    });
+});
